51290cdd6c14f56a82b8571f73b90d5f57feb2af
gracemontagnino <gmontagnino@olin.edu>
2019-11-22 11:57:02 -0500

    shiftrows works

--- shiftrows.t.v
+++ shiftrows.t.v
@@ -9,21 +9,21 @@ module test_shiftrows();
   initial begin
   $display("Shift Rows test");
   state={8'b00000000, 8'b00000001, 8'b00000010, 8'b00000011, 8'b00000100, 8'b00000101,8'b00000111,8'b00001000,8'b00001001,8'b00001010,8'b00000000,8'b10000000,8'b11000000,8'b11100000,8'b11110000,8'b11111001}; #1000
-  $display("%b",state[0]);
-  $display("%b",state[1]);
-  $display("%b",state[2]);
-  $display("%b",state[3]);
-  $display("%b",state[4]);
-  $display("%b",state[5]);
-  $display("%b",state[6]);
-  $display("%b",state[7]);
-  $display("%b",state[8]);
-  $display("%b",state[9]);
-  $display("%b",state[10]);
-  $display("%b",state[11]);
-  $display("%b",state[12]);
-  $display("%b",state[13]);
-  $display("%b",state[14]);
-  $display("%b",state[15]);
+  $display("%b",newstate[0]);
+  $display("%b",newstate[1]);
+  $display("%b",newstate[2]);
+  $display("%b",newstate[3]);
+  $display("%b",newstate[4]);
+  $display("%b",newstate[5]);
+  $display("%b",newstate[6]);
+  $display("%b",newstate[7]);
+  $display("%b",newstate[8]);
+  $display("%b",newstate[9]);
+  $display("%b",newstate[10]);
+  $display("%b",newstate[11]);
+  $display("%b",newstate[12]);
+  $display("%b",newstate[13]);
+  $display("%b",newstate[14]);
+  $display("%b",newstate[15]);
   end
 endmodule
--- shiftrows.v
+++ shiftrows.v
@@ -18,15 +18,15 @@ assign newstate[8][7:0]=state[11][7:0];
 
 
 //row 3 shifts by 2
-assign newstate[7][7:0]=state[][7:0];
-assign newstate[6][7:0]=state[][7:0];
-assign newstate[5][7:0]=state[][7:0];
-assign newstate[4][7:0]=state[][7:0];
+assign newstate[7][7:0]=state[5][7:0];
+assign newstate[6][7:0]=state[4][7:0];
+assign newstate[5][7:0]=state[7][7:0];
+assign newstate[4][7:0]=state[6][7:0];
 
 
 //row 4 shifts by 3
-assign newstate[3][7:0]=state[][7:0];
-assign newstate[2][7:0]=state[][7:0];
-assign newstate[1][7:0]=state[][7:0];
-assign newstate[0][7:0]=state[][7:0];
+assign newstate[3][7:0]=state[0][7:0];
+assign newstate[2][7:0]=state[3][7:0];
+assign newstate[1][7:0]=state[2][7:0];
+assign newstate[0][7:0]=state[1][7:0];
 endmodule
